c4993a8a62d4d6a78999e9a5053a23f9111001fe
Edson Candido <edson.cursoprogramacao@gmail.com>
2020-08-02 20:54:48 -0300

    Formulario de cadastro de usuários

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Inicio from './pages/client/painel';
+import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <Inicio />
+      <Routes />
     </div>
   );
 }
